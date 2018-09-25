f1dfd429037699f48652a45f105ef0eb0c463e2c
Bianquezi <psn.ads.ifsp@gmail.com>
2018-09-25 11:37:35 -0300

    reconfiguração de email

--- model/Mensagem.php
+++ model/Mensagem.php
@@ -7,7 +7,9 @@
  */
 
     // Inclui o arquivo class.phpmailer.php localizado na pasta class
-    include_once "../aux/PHPMailer-6.0.5/src/PHPMailer.php";
+  //  require_once "../aux/stmp_mail/class.phpmailer.php";
+    require "../aux/stmp_mail/PHPMailerAutoload.php";
+  //  require_once "../aux/stmp_mail/class.smtp.php";
 
 class Mensagem
 {
@@ -27,48 +29,33 @@ class Mensagem
     public function enviarMensagem()
     {
         // Inicia a classe PHPMailer
-        $mail = new PHPMailer(true);
+        $mail = new PHPMailer;
+        $mail->isSMTP(true);
+
         // Define os dados do servidor e tipo de conexão
         // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        $mail->IsSMTP(true); // Define que a mensagem será SMTP
-        $mail->SMTPDebug  = 1;
         $mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP
         $mail->Port = 587;
         $mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
-        $mail->SMTPSecure = 'ssl';
+        $mail->SMTPSecure = 'tls';
+
         $mail->Username = 'olhar.cidadao.ifsp@gmail.com'; // Usuário do servidor SMTP
         $mail->Password = 'ifspifsp'; // Senha do servidor SMTP
-        // Define o remetente
-        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        $mail->From = "olhar.cidadao.ifsp@gmail.com"; // Seu e-mail
-        $mail->FromName = "Olhar Cidadão"; // Seu nome
-        // Define os destinatário(s)
-        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        $mail->AddAddress('olhar.cidadao.ifsp@gmail.com', 'Teste');
-        // Define os dados técnicos da Mensagem
-        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-        // Define a mensagem (Texto e Assunto)
-        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        $mail->Subject  = "Mensagem Teste"; // Assunto da mensagem
-        $mail->Body = "Este é o corpo da mensagem de teste, em <b>HTML</b>!  :)";
-        $mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n :)";
-        // Define os anexos (opcional)
-        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        //$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
-        // Envia o e-mail
-        $enviado = $mail->Send();
-        // Limpa os destinatários e os anexos
-        $mail->ClearAllRecipients();
-        $mail->ClearAttachments();
-        // Exibe uma mensagem de resultado
-        if ($enviado) {
-            echo "E-mail enviado com sucesso!";
+
+        $mail->setFrom("olhar.cidadao.ifsp@gmail.com", "Olhar Cidadão");
+        $mail->addAddress('TESTE 01');
+        $mail->addReplyTo('olhar.cidadao.ifsp@gmail.com');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'PHP TESTE OLHAR CIDADAO';
+        $mail->Body = '<h5 align="center">TESTE</h5>';
+
+        if(!$mail->send()){
+            echo "Email não foi enviado";
+        }else{
+            echo "Email enviado com sucesso!";
         }
-        else {
-            echo "Não foi possível enviar o e-mail.";
-            echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
-        }
+
     }
 
 
